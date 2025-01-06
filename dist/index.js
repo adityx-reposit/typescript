715a03edb8c7b1eb0da759d469780f1357c6ff8d
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-01-06 21:03:26 +0530

    typescript initial

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,13 @@
 "use strict";
-// function sum(number1:number,number2:number){
-// return number1+number2;
-// }
-let people = {
+const prople = {
     name: "aditya",
     age: 21,
     greet: () => {
-        return "hi " + people.name;
+        return "hi";
+    },
+    greet2: () => {
+        return "hello";
     }
 };
-// let greeting=people.greet();
-// console.log(greeting);
-// class Manager implements People{
-//     name:string;
-//     age:number;
-//    constructor(name:string,age:number){
-//        this.name= name;
-//        this.age= age;
-//    }
-// }
-// let user=new Manager("aditya",21);
-// console.log(user.name+" "+ user.age);
+console.log(prople.greet());
+console.log(prople.greet2());
