2fb6bb701407c095cf19f9d8cfe34ec307bd064e
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-01-07 23:58:49 +0530

    day2

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,38 @@
 "use strict";
-function sumofAge(user1, user2) {
-    return (user1.age + user2.age);
-}
-;
-const result = sumofAge({
-    name: "aditya",
-    age: 21
-}, {
-    name: "harkirat",
-    age: 32
-});
-console.log(result);
+// interface User{
+//     id:string,
+//     name:string,
+//     age:number,
+//     email:string,
+//     password:string
+// }only
+// type User={
+//      name:string,
+//      age:number,
+//      country:string
+// }
+// const obj: Readonly<User>={
+//     name:"aditya",
+//     age:21,
+//     country:"india"
+// }
+// type UpdateProps= Pick<User,'name'| 'age'|'email'>
+// type UpdatePropsOption = Partial<UpdateProps>
+// function updateUser(updatedProps:UpdatePropsOption){
+// }
+const users = new Map();
+users.set("admin1", { name: "aditya", age: 32, country: "india" });
+users.set("admin2", { name: "aditya", age: 21, country: "india" });
+console.log(users.get("admin1"));
+// const user:Users={
+//     "ras@qd1":{
+//         name:"aditya",
+//         age:32,
+//         country:"india"
+//     },
+//     "ras@qd2":{
+//         name:"aditya",
+//         age:21,
+//         country:"india"
+//     }
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-interface User{
-    id:string,
-    name:string,
-    age:number,
-    email:string,
-    password:string
-}
-type UpdateProps= Pick<User,'name'| 'age'|'email'>
-type UpdatePropsOption = Partial<UpdateProps>
-function updateUser(updatedProps:UpdateProps){
+// interface User{
+//     id:string,
+//     name:string,
+//     age:number,
+//     email:string,
+//     password:string
+// }only
+// type User={
+//      name:string,
+//      age:number,
+//      country:string
+// }
+// const obj: Readonly<User>={
+//     name:"aditya",
+//     age:21,
+//     country:"india"
+// }
+
+   
+// type UpdateProps= Pick<User,'name'| 'age'|'email'>
+// type UpdatePropsOption = Partial<UpdateProps>
+// function updateUser(updatedProps:UpdatePropsOption){
        
-}
+// }
+
+const users=new Map<string,{name:string,age:number,country:string}>()
+    users.set("admin1",{name:"aditya", age:32,country:"india"})
+    users.set("admin2",{name:"aditya", age:21,country:"india"})
+
+console.log(users.get("admin1"));
+
+
+
+// const user:Users={
+//     "ras@qd1":{
+//         name:"aditya",
+//         age:32,
+//         country:"india"
+//     },
+//     "ras@qd2":{
+//         name:"aditya",
+//         age:21,
+//         country:"india"
+//     }
+// }
+
